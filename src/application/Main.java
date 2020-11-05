d3b31da3b5d28be7fcfeedf22ca227d5ceed49bf
Marck <maanmuri@espol.edu.ec>
2020-11-04 23:33:54 -0500

    ventana con botones

---
+++ src/application/Main.java
@@ -0,0 +1,69 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+
+		//fondo para encajar a los botones
+        HBox root = new HBox(60);
+        root.setAlignment(Pos.CENTER);
+        
+        //Botones
+        Button  boton1 = new Button("ROJO");
+        Button  boton2 = new Button("AZUL");
+        Button  boton3 = new Button("AMARILLO");
+        root.getChildren().addAll(boton1, boton2, boton3);  
+        
+        //Acciones de los botones
+        boton1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+            	root.setBackground(new Background(new BackgroundFill(Color.RED,null,null)));
+            }
+        });
+        
+        
+        boton2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+            	root.setBackground(new Background(new BackgroundFill(Color.BLUE,null,null)));
+            }
+        });
+        
+        
+        boton3.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+            	root.setBackground(new Background(new BackgroundFill(Color.YELLOW,null,null)));
+            }
+        });
+        
+        
+        //Configuraciones de la ventana
+        Scene scene = new Scene(root, 500, 300);
+        primaryStage.setTitle("TAREA 01");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
